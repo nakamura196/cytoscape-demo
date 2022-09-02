d1f50273551fbfb946cae5ff347469e7d2e07477
Satoru <you@example.com>
2022-09-02 18:36:31 +0900

    feat: initial commit

---
+++ app.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <!-- Markup shared across all pages, ex: NavBar -->
+    <!-- aaa -->
+    <NuxtPage />
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-container>
+    
+    <v-row>
+      <v-col cols="6">
+        <v-card variant="outlined">
+          <div class="pa-4">
+            
+            <nuxt-link to="/example1">
+              基本の表示
+            </nuxt-link>
+            <!--
+            -->
+          </div>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+<!--
+<script lang="ts">
+export default defineComponent({
+  setup() {
+    
+
+    // 変数・算出プロパティ・メソッドを返却 ...（7）
+    return {
+    };
+  },
+});
+</script>
+-->
